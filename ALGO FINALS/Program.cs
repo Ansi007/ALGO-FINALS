4ee4b2d48d5ad28578ff9f5d53dcd453f3700134
Anns Shahbaz <annsshahbaz@hotmail.com>
2022-01-30 12:34:35 +0500

    HW Started

--- ALGO FINALS/Program.cs
+++ ALGO FINALS/Program.cs
@@ -1,4 +1,140 @@
-﻿
+﻿public class HomeWork01
+{
+    void p1Searching(int[] A, int X)
+    {
+        /*
+            Sort the array,
+        int i = 0; int j = n - 1;
+        while(i < j){
+         if(A[i]+A[j]==X) return i,j;
+         else if(A[i]+A[j]<X) return i++;
+         else return j--;
+
+         */
+    }
+
+    void p2Decision(int[] S1, int[] S2)
+    {
+        /*
+            Sort the sets (nlogn)
+        int i = 0; int j = 0;
+        while(i < j){
+         if(S1[i]==S2[j]) return false;
+         else if(S1[i]<S2[j]) return i++;
+         else return j--;
+         */
+    }
+
+        int p4Searching(int[] arr, int low, int high)
+        {
+        // This condition is needed to handle the case
+        // when the array is not rotated at all
+        if (high < low)
+            return 0;
+
+        // If there is only one element left
+        if (high == low)
+            return low;
+
+        // Find mid
+        int mid = low + (high - low) / 2; /*(low + high)/2;*/
+
+        // Check if element (mid+1) is minimum element.
+        // Consider the cases like {3, 4, 5, 1, 2}
+        if (mid < high && arr[mid + 1] < arr[mid])
+            return (mid + 1);
+
+        // Check if mid itself is minimum element
+        if (mid > low && arr[mid] < arr[mid - 1])
+            return mid;
+
+        // Decide whether we need to go to left half or
+        // right half
+        if (arr[high] > arr[mid])
+            return p4Searching(arr, low, mid - 1);
+
+        return p4Searching(arr, mid + 1, high);
+    }
+}
+
+
+
+
+
+
+
+public class MaxHeap
+{
+
+/*
+ 
+    Start from 2nd Last level, from right to left and heapify every node.
+    e.g: 1 2 3 4 5 6 7
+    int i = ceil(Length(A)/2) to 1
+            1
+        2       3
+      4   5   6   7
+
+ */
+
+
+    public static void BuildMaxHeapIterative(ref int[] array)
+    {
+        for(int i = (array.Length / 2)-1; i >= 0; i--)
+        {
+            HeapifyIterative(ref array, i);
+        }
+    }
+
+    static void HeapifyIterative(ref int[] array, int i)
+    {
+        int LC = 2 * i + 1;
+        int RC = 2 * i + 2;
+        int max = 0;
+        int max_index = 0;
+        int n = array.Length;
+        int LCV = -1, RCV = -1;
+
+        if (RC < n)
+            RCV = array[RC];
+
+        if(LC < n)
+            LCV = array[LC];
+
+        if (LCV > RCV)
+        {
+            if (LCV > array[i])
+            {
+                max = LCV;
+                max_index = LC;
+            }
+            else
+            {
+                max = array[i];
+                max_index = i;
+            }
+        }
+        else
+        {
+            if (RCV > array[i])
+            {
+                max = RCV;
+                max_index = RC;
+            }
+            else
+            {
+                max = array[i];
+                max_index = i;
+            }
+        }
+
+        int temp = array[i];
+        array[i] = max;
+        array[max_index] = temp;
+
+
+    }
+}
 
 
 
