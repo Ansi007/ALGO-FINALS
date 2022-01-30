fe09315e93ab725f450c13c11224cfd55254f073
Anns Shahbaz <annsshahbaz@hotmail.com>
2022-01-30 13:59:17 +0500

    HomeWork 02 A Completed Problem 05 Left

--- ALGO FINALS/Program.cs
+++ ALGO FINALS/Program.cs
@@ -1,4 +1,4 @@
-﻿public class HomeWork01
+﻿public class HomeWork02
 {
     void p1Searching(int[] A, int X)
     {
@@ -25,7 +25,97 @@
          */
     }
 
-        int p4Searching(int[] arr, int low, int high)
+    void p3MaxSubArray(int[] a, int size)
+    {
+        int max_so_far = int.MinValue,
+        max_ending_here = 0, start = 0,
+        end = 0, s = 0;
+
+
+        // 1 5 -2 4 5 -4 2 -10
+
+        // -2 -3 4 -1 -2 1 5 -3
+
+        for (int i = 0; i < size; i++)
+        {
+            max_ending_here += a[i];
+
+            if (max_so_far < max_ending_here)
+            {
+                max_so_far = max_ending_here;
+                start = s;  // 0 0 0 0
+                end = i;    // 0 1 3 4
+            }
+
+            if (max_ending_here < 0)
+            {
+                s = i + 1; //2
+                max_ending_here = 0;
+            }
+        }
+        //return start,end
+        //O(n)
+    }
+
+    static int maxCrossingSum(int[] arr, int l, int m,
+                              int h)
+    {
+        // Include elements on left of mid.
+        int sum = 0;
+        int left_sum = int.MinValue;
+        for (int i = m; i >= l; i--)
+        {
+            sum = sum + arr[i];
+            if (sum > left_sum)
+                left_sum = sum;
+        }
+
+        // Include elements on right of mid
+        sum = 0;
+        int right_sum = int.MinValue;
+        ;
+        for (int i = m + 1; i <= h; i++)
+        {
+            sum = sum + arr[i];
+            if (sum > right_sum)
+                right_sum = sum;
+        }
+
+        // Return sum of elements on left
+        // and right of mid
+        // returning only left_sum + right_sum will fail for
+        // [-2, 1]
+        return Math.Max(left_sum + right_sum,
+                        Math.Max(left_sum, right_sum));
+    }
+
+    // Returns sum of maximum sum subarray
+    // in aa[l..h]
+    int p3maxSubArraySum(int[] arr, int l, int h)
+    {
+
+        // Base Case: Only one element
+        if (l == h)
+            return arr[l];
+
+        // Find middle point
+        int m = (l + h) / 2;
+
+        /* Return maximum of following three
+        possible cases:
+        a) Maximum subarray sum in left half
+        b) Maximum subarray sum in right half
+        c) Maximum subarray sum such that the
+        subarray crosses the midpoint */
+        return Math.Max(
+            Math.Max(p3maxSubArraySum(arr, l, m),
+                     p3maxSubArraySum(arr, m + 1, h)),
+            maxCrossingSum(arr, l, m, h));
+    }
+
+
+
+    int p4Searching(int[] arr, int low, int high)
         {
         // This condition is needed to handle the case
         // when the array is not rotated at all
@@ -55,6 +145,55 @@
 
         return p4Searching(arr, mid + 1, high);
     }
+
+
+    /*
+     
+    Problem 6: Design an array, the array is:
+     // 18 19 20 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17
+     
+     */
+
+
+
+    /*
+            Problem 7:
+
+    Sort(A,i,j){
+        Find random pivot
+        Call Partition(A,i,j,pivot)
+        Partition will return us IndexStart, IndexEnd
+        Call Sort(A,i,IndexStart - 1)
+        Call Sort(A,IndexEnd + 1,j)
+    }
+
+    Partition(A,i,j,pivot){
+        First find how many repeated pivots elements are there store them in count
+        Then remove other repeated elements except the pivot
+        Change pivot = new Pivot Index of modifed Array
+        Do the previous simple Parition Procedure and find the ideal position of pivot
+        Then re insert those removed elements after pivot
+        Return pivot, pivot + count
+    } 
+     */
+
+
+
+
+
+    /*
+     
+    Problem 8:
+        ElementUnique(A){
+            if(FindMode(A) == 0) //O(nlogn)
+                return true;
+            else return false;
+    }
+
+    It is stated that ElementUnique best case is O(nlogn),FindMode will also be
+    O(nlogn)
+     
+   */
 }
 
 
@@ -263,6 +402,8 @@ public class QuickSort
     Then Quick Sort is applied on left and right array excluding the already sorted index
  
  */
+
+   
     public static void Sort(ref int[] array, int i, int j)
     {
 
@@ -308,6 +449,91 @@ public class QuickSort
         temp = array[i]; //sawp ith element with qth and return q
         array[i] = array[q];
         array[q] = temp;
+        return q;
+    }
+
+
+}
+
+
+public class QuickSortModifiedForRepeatElements
+{
+
+
+    public static void Sort(ref int[] array, int i, int j)
+    {
+
+        if (i == j || i < 0 || j < 0 || j < i)
+            return; //Base Case
+
+        int pivot = new Random().Next(i, j);        //Random Pivot
+        int index = Partition(ref array, i, j, pivot); //Paritioning and finding its index
+        Sort(ref array, i, index - 1);  //Sorting left array
+        Sort(ref array, index + 1, j);  //Sorting right array
+    }
+
+    static int Partition(ref int[] array, int i, int j, int pivotIndex)
+    {
+        int count = -1;
+        for(int I = i; I <= j; I++)
+        {
+            if (array[i] == array[pivotIndex])
+                count++;
+        }
+
+        int[] tempArray = new int[(j - i + 1) - count];
+        int k = 0;
+        for (int I = i; I <= j; I++)
+        {
+            if (array[i] == array[pivotIndex] && i == pivotIndex)
+                tempArray[k++] = array[i];
+
+            else if (array[i] == array[pivotIndex] && i == pivotIndex)
+                continue;
+
+            else tempArray[k++] = array[i];
+
+        }
+
+
+        int temp = tempArray[i];        //Swaping first element with pivot
+        tempArray[i] = tempArray[pivotIndex];
+        tempArray[pivotIndex] = temp;
+
+        int p = i + 1;
+        int q = j;
+
+        while (p <= q)
+        {
+            if (tempArray[p] < tempArray[i]) //While array[p] is smaller than array[i] p++;
+            {
+                p++;
+            }
+
+            else if (tempArray[q] > tempArray[i])    //While array[q] is larger than array[i] q--;
+            {
+                q--;
+            }
+
+            else //Else swap qth and pth elements
+            {
+                int temp1 = tempArray[p];
+                tempArray[p] = tempArray[q];
+                tempArray[q] = temp1;
+                p++;
+                q--;
+            }
+
+        }
+        temp = tempArray[i]; //sawp ith element with qth and return q
+        tempArray[i] = tempArray[q];
+        tempArray[q] = temp;
+
+        for(int I  = 0; I < count; I++)
+        {
+            //Re insert tempArray[q] between q and the next index
+        }
+        //then return q and q + count
         return q;
     }
 
